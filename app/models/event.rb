c4d83b8156a6c234a80935e09d59144154988667
楊竑昕 <jk195417@gmail.com>
2016-07-01 01:01:54 +0800

    bug fix
    timezone
    profile valid
    event valid

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,10 +6,14 @@ class Event < ActiveRecord::Base
 	# event has many members from user, and save in group_user
 	# 刪除 event 會清空報名event的人的資料(participants)
 	has_many :event_users
-    has_many :participants, through: :event_users, source: :user, dependent: :destroy
+  has_many :participants, through: :event_users, source: :user, dependent: :destroy
 	
+	# 驗證
+	# 必填欄位
+  validates_presence_of  :name, :max_sign_up_number, :sign_up_begin, :sign_up_end, :start, :over
+
 	# 修改權限
 	def editable_by?(user)
-    	user && user == organizer
+    user && user == organizer
     end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ActiveRecord::Base
 	belongs_to :user
+
+	# 必填欄位
+  validates_presence_of  :name, :sex, :birth, :email
+  # email 正規表示法
+  validates_format_of :email, :with => /\A\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+\z/
 end
