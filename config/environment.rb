32c255b624596b71447fc837bdcbb444411d8ee6
楊竑昕 <jk195417@gmail.com>
2017-01-07 17:37:10 +0800

    heroic sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# heroku sendgrid mailer
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.sendgrid.net',
+  port:                 587,
+  domain:               'heroku.com',
+  user_name:            ENV['SENDGRID_USERNAME'],
+  password:             ENV['SENDGRID_PASSWORD'],
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,16 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # mailer
-  config.action_mailer.default_url_options = { host: 'https://caten-church.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 587,
-    domain:               'heroku.com',
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
-    authentication:       :plain,
-    # enable_starttls_auto: true
-  }
 end
