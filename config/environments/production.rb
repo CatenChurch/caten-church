92e7c2ffd1d7277ed7503f3f2f37801e1c49f742
楊竑昕 <jk195417@gmail.com>
2017-01-07 16:54:38 +0800

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'heroku.com',
     user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
