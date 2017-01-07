fec2cb1be489e57235b4e0ab74a2f8fd9ba30631
楊竑昕 <jk195417@gmail.com>
2017-01-07 17:15:39 +0800

    mail set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,6 @@ Rails.application.configure do
 
   # mailer
   config.action_mailer.default_url_options = { host: 'https://caten-church.herokuapp.com', from: ENV['SENDGRID_USERNAME'] }
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.com',
