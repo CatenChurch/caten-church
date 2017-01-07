9c5bfd85591bf384d37caa23bcb98f20283317c5
楊竑昕 <jk195417@gmail.com>
2017-01-07 17:22:44 +0800

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://caten-church.herokuapp.com', from: ENV['SENDGRID_USERNAME'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
     domain:               'heroku.com',
     user_name:            ENV['SENDGRID_USERNAME'],
