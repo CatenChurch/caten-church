4b39c3f20ec8448b416a7dc73733fa3a022de634
楊竑昕 <jk195417@gmail.com>
2017-01-07 15:43:28 +0800

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,6 @@ Rails.application.configure do
 
   # mailer
   config.action_mailer.default_url_options = { host: 'https://caten-church.herokuapp.com' }
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
