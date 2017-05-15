38eacd7769b6b4c2280bf73ae701ee4bba823f47
楊竑昕 <jk195417@gmail.com>
2017-05-16 01:21:47 +0800

    Admin EventUser show edit update

---
+++ app/controllers/admin/event_users_controller.rb
@@ -0,0 +1,40 @@
+class Admin::EventUsersController < AdminController
+  before_action :set_event_users, only: [:edit, :show, :update]
+  def edit; end
+
+  def show; end
+
+  def update
+    query_success = true
+    @event_users.each do |e|
+      id = e.id.to_s
+      dig_result = event_users_params.dig(id, 'activated')
+      if dig_result == '1' && !e.activated
+        e.activated = true
+        query_success = (e.save || query_success)
+      elsif dig_result == '0' && e.activated
+        e.activated = false
+        query_success = (e.save || query_success)
+      end
+    end
+    # 這行會執行每個EventUser的SQL，即使沒有更新
+    # query_success = @event_users.update(event_users_params.keys, event_users_params.values)
+    if query_success
+      flash[:notice] = 'Successfully updated event_users.'
+      redirect_to admin_event_event_users_url
+    else
+      flash[:warning] = 'Updated event_users failed.'
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_event_users
+    @event_users = EventUser.where(event_id: params[:event_id]).includes(user: :profile).order(:id)
+  end
+
+  def event_users_params
+    params.require(:event_users).permit!
+  end
+end
