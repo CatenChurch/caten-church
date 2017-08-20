9a41cc7ccdf3701e3be502cb9f59b909aca1caf9
楊竑昕 <jk195417@gmail.com>
2017-08-20 22:28:21 +0800

    user service_team relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,13 +20,6 @@
 #
 
 class User < ApplicationRecord
-  scope :birth_month, ->(month = '') {
-    return all if month.blank?
-    user_ids = Profile.birth_month(month).map(&:user_id)
-    where(id: user_ids)
-  }
-
-  # roles
   rolify
 
   # Include default devise modules. Others available are:
@@ -36,18 +29,24 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:facebook]
 
   has_one :profile, dependent: :destroy
-
   has_many :announcements
-
-  has_many :events
-  has_many :event_users
+  has_many :events, dependent: :nullify
+  has_many :event_users, dependent: :destroy
   has_many :participated_events, through: :event_users, source: :event
-
   has_many :oauths, dependent: :destroy
+  has_many :service_teams, class_name: Service::Team
+  has_many :service_team_user, class_name: Service::TeamUser
+  has_many :serviced_teams, through: :service_team_user, source: :team, class_name: Service::Team
 
   # ransackable_scopes https://github.com/activerecord-hackery/ransack#using-scopesclass-methods
   def self.ransackable_scopes(_auth_object = nil)
     [:birth_month]
+  end
+
+  def self.birth_month(month = nil)
+    return all if month.blank?
+    user_ids = Profile.birth_month(month).pluck(:user_id)
+    where(id: user_ids)
   end
 
   # user has profile
