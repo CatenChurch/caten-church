54558e418c501c613f0d3b1dd6e3c724dddb58f0
楊竑昕 <jk195417@gmail.com>
2017-10-09 16:15:00 +0800

    send birth notification job

---
+++ app/jobs/admin/send_birth_notification_to_admin_job.rb
@@ -0,0 +1,10 @@
+class Admin::SendBirthNotificationToAdminJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    month = (Time.now + 1.months).month
+    User.with_role(:admin).each do |admin|
+      UserMailer.birth_notification(admin, month).deliver_later
+    end
+  end
+end
