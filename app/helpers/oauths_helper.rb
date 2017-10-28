c7602c958fef54a5d2b9eba9b36eaa57b487a67d
楊竑昕 <jk195417@gmail.com>
2017-10-28 12:11:59 +0800

    facebook google instagram link helper

---
+++ app/helpers/oauths_helper.rb
@@ -0,0 +1,25 @@
+module OauthsHelper
+  def connect_facebook_link(user)
+    if user.facebook_oauth.present?
+      link_to "https://www.facebook.com/#{current_user.facebook_oauth.uid}", target: :_blank, class: 'btn btn-outline-primary list-group-item' do
+        fa_icon('facebook-official 3x', text: '連結成功')
+      end
+    else
+      link_to user_facebook_omniauth_authorize_path, class: 'btn btn-outline-primary list-group-item' do
+        fa_icon('facebook-official 3x', text: '開始連結')
+      end
+    end
+  end
+
+  def connect_google_link(user)
+    link_to "#google-login-is-coming", class: 'btn list-group-item disabled' do
+      fa_icon('google 3x', text: '即將推出')
+    end
+  end
+
+  def connect_instagram_link(user)
+    link_to "#instagram-login-is-coming", class: 'btn list-group-item disabled' do
+      fa_icon('instagram 3x', text: '即將推出')
+    end
+  end
+end
