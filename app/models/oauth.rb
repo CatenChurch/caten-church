476d6ac32871630170e4cc068625bba9f3d3509a
楊竑昕 <jk195417@gmail.com>
2017-10-28 12:11:27 +0800

    oauth provider and user_id uniq validate

--- app/models/oauth.rb
+++ app/models/oauth.rb
@@ -1,7 +1,10 @@
 class Oauth < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of  :provider, :uid
+  validates_presence_of :provider, :uid
+  # db schema 有加 uniq key 限制，故此應用層的驗證可註解掉
+  # validates_uniqueness_of :provider, scope: :uid
+  validates_uniqueness_of :user_id, scope: :provider, if: Proc.new { |oauth| oauth.user_id.present? }
 
   def self.find_or_create_user(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |oauth|
