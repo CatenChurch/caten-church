803b3f9ad6007a8cead8228fc112f9a910835165
楊竑昕 <jk195417@gmail.com>
2017-11-05 16:09:24 +0800

    update chatbot

--- app/controllers/chatbot/base_controller.rb
+++ app/controllers/chatbot/base_controller.rb
@@ -1,7 +1,3 @@
 class Chatbot::BaseController < ApplicationController
   protect_from_forgery with: :null_session
-
-  def verification
-    render plain: 'Success', status: 200
-  end
 end
--- app/controllers/chatbot/lines_controller.rb
+++ app/controllers/chatbot/lines_controller.rb
@@ -1,2 +1,9 @@
 class Chatbot::LinesController < Chatbot::BaseController
+  def callback
+    if params['hub.mode'] == 'subscribe' && params['hub.verify_token'] == ENV['FB_MESG_TOKEN']
+      render plain: params['hub.challenge'], status: 200
+    else
+      render plain: 'wrong verify token', status: 404
+    end
+  end
 end
