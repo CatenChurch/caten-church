7243bd7d5a39506a9315a1d1d8e346e84e4a9fe0
楊竑昕 <jk195417@gmail.com>
2017-11-05 20:30:58 +0800

    echo

--- app/controllers/chatbot/lines_controller.rb
+++ app/controllers/chatbot/lines_controller.rb
@@ -1,5 +1,69 @@
 class Chatbot::LinesController < Chatbot::BaseController
+  require 'line/bot'
+  before_action :setup_line_bot
+  before_action :valid_signature
+
   def callback
+    # line will post something like this
+    # {
+    #   "events": [
+    #     {
+    #       "type": "message",
+    #       "replyToken": "16456834b271405a896a13c4692cf8e3",
+    #       "source": {
+    #         "userId": "Ub38d4062c864a494a3229bc077a6551d",
+    #         "type": "user"
+    #       },
+    #       "timestamp": 1509884468226,
+    #       "message": {
+    #         "type": "text",
+    #         "id": "6945380161618",
+    #         "text": "hi"
+    #       }
+    #     }
+    #   ],
+    #   "line": {
+    #     "events": [
+    #       {
+    #         "type": "message",
+    #         "replyToken": "16456834b271405a896a13c4692cf8e3",
+    #         "source": {
+    #           "userId": "Ub38d4062c864a494a3229bc077a6551d",
+    #           "type": "user"
+    #         },
+    #         "timestamp": 1509884468226,
+    #         "message": {
+    #           "type": "text",
+    #           "id": "6945380161618",
+    #           "text": "hi"
+    #         }
+    #       }
+    #     ]
+    #   }
+    # }
+    conversations = @bot.parse_events_from(request.raw_post)
+    conversations.each do |conversation|
+      next unless conversation.is_a(Line::Bot::Event::Message) && conversation.type.is_a(Line::Bot::Event::MessageType::Text)
+      message = { type: 'text', text: conversation.message['text'] }
+      @bot.reply_message(conversation['replyToken'], message)
+    end
     render plain: 'success', status: 200
+  end
+
+  private
+
+  def setup_line_bot
+    @bot = Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+
+  def valid_signature
+    data = request.raw_post
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless @bot.validate_signature(data, signature)
+      render plain: 'signature not valid', status: 400
+    end
   end
 end
