aa5cb13161b6158355f8107a42e426e3ff42ab45
楊竑昕 <jk195417@gmail.com>
2018-02-12 12:34:55 +0800

    disable clustered mode, using single mode.

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,19 @@
 # Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers a minimum and maximum.
+# The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum, this matches the default thread size of Active Record.
+# and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch('RAILS_ENV') { 'development' }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -21,7 +21,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# workers ENV.fetch('WEB_CONCURRENCY') { 2 }.to_i
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -30,33 +30,27 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-preload_app!
+# preload_app!
 
-# no need for new puma version
-# rackup DefaultRackup
-
-# Because we are using preload_app, an instance of our app is created by master process (calling our initializers) and then memory space
-# is forked. So we should close DB connection in the master process to avoid connection leaks.
-# https://github.com/puma/puma/issues/303
-# http://stackoverflow.com/questions/17903689/puma-cluster-configuration-on-heroku
-# http://www.rubydoc.info/gems/puma/2.14.0/Puma%2FDSL%3Abefore_fork
-# Dont have to worry about Sidekiq's connection to Redis because connections are only created when needed. As long as we are not
-# queuing workers when rails is booting, there will be no redis connections to disconnect, so it should be fine.
-before_fork do
-  Rails.logger.debug { 'Puma master process about to fork. Closing existing Active record connections.' }
-  ActiveRecord::Base.connection.disconnect!
-end
+# If you are preloading your application and using Active Record, it's
+# recommended that you close any connections to the database before workers
+# are forked to prevent connection leakage.
+#
+# before_fork do
+#   ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+# end
 
 # The code in the `on_worker_boot` will be called if you are using
 # clustered mode by specifying a number of `workers`. After each worker
-# process is booted this block will be run, if you are using `preload_app!`
-# option you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, Ruby
+# process is booted, this block will be run. If you are using the `preload_app!`
+# option, you will want to use this block to reconnect to any threads
+# or connections that may have been created at application boot, as Ruby
 # cannot share connections between processes.
 #
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+# end
+#
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
