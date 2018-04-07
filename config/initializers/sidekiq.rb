0775a670211757aa0ae46ff30e7b49efef1ba048
楊竑昕 <jk195417@gmail.com>
2018-04-07 13:10:14 +0800

    Initializer: sidekiq comments

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,21 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
+
+# Set Redis url to ENV['REDIS_PROVIDER'], and Sidekiq will use it without spec
+# Redis.new(url: ENV['REDIS_PROVIDER']) = Redis.new if ENV['REDIS_PROVIDER'] have value
+# read: https://github.com/mperham/sidekiq/wiki/Using-Redis#using-an-env-variable
+
+
+# Connection pool with Redis
+# read: https://github.com/mperham/sidekiq/wiki/Using-Redis#complete-control
+#
+# redis_conn = proc {
+#   Redis.new(url: ENV['REDIS_URL'])
+# }
+# Sidekiq.configure_client do |config|
+#   config.redis = ConnectionPool.new(size: 5, &redis_conn)
+# end
+# Sidekiq.configure_server do |config|
+#   config.redis = ConnectionPool.new(size: 25, &redis_conn)
+# end
