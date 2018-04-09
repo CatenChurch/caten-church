a570481470cb43d5cfad422ae986a2b4262aeee3
楊竑昕 <jk195417@gmail.com>
2018-04-09 11:51:47 +0800

    View: i18n, helper refractory

--- app/helpers/admin/contacts_helper.rb
+++
@@ -1,5 +0,0 @@
-module Admin::ContactsHelper
-  def handle_status(boolean)
-    boolean ? t('contact.handle.y') : t('contact.handle.n')
-  end
-end
--- app/helpers/admin/event_users_helper.rb
+++
@@ -1,9 +0,0 @@
-module Admin::EventUsersHelper
-  def paid(boolean)
-    boolean ? t('event_user.paid.y') : t('event_user.paid.n')
-  end
-
-  def arrival(boolean)
-    boolean ? t('event_user.arrival.y') : t('event_user.arrival.n')
-  end
-end
