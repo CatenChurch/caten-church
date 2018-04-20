bf43b4784fbb4efe04aa8231b3a879656f8e4cfc
楊竑昕 <jk195417@gmail.com>
2018-04-20 15:31:32 +0800

    Subscription: migration, schema, model and empty tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,169 +10,180 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001133827) do
+ActiveRecord::Schema.define(version: 20180420064125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "announcements", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.text     "content"
-    t.integer  "user_id"
+  create_table "announcements", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "content"
+    t.integer "user_id"
   end
 
-  create_table "contacts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "phone"
-    t.text     "message"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "handled",    default: false
-    t.index ["handled"], name: "index_contacts_on_handled", using: :btree
+  create_table "contacts", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "handled", default: false
+    t.index ["handled"], name: "index_contacts_on_handled"
   end
 
-  create_table "event_users", force: :cascade do |t|
-    t.integer  "event_id"
-    t.integer  "user_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "paid",       default: false
-    t.boolean  "arrival",    default: false
-    t.string   "remark"
-    t.index ["event_id", "user_id"], name: "index_event_users_on_event_id_and_user_id", unique: true, using: :btree
-    t.index ["event_id"], name: "index_event_users_on_event_id", using: :btree
-    t.index ["user_id"], name: "index_event_users_on_user_id", using: :btree
+  create_table "event_users", id: :serial, force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "paid", default: false
+    t.boolean "arrival", default: false
+    t.string "remark"
+    t.index ["event_id", "user_id"], name: "index_event_users_on_event_id_and_user_id", unique: true
+    t.index ["event_id"], name: "index_event_users_on_event_id"
+    t.index ["user_id"], name: "index_event_users_on_user_id"
   end
 
-  create_table "events", force: :cascade do |t|
-    t.string   "name"
-    t.string   "nature"
-    t.text     "description"
-    t.integer  "max_sign_up_number"
-    t.integer  "min_sign_up_number"
+  create_table "events", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "nature"
+    t.text "description"
+    t.integer "max_sign_up_number"
+    t.integer "min_sign_up_number"
     t.datetime "sign_up_begin"
     t.datetime "sign_up_end"
     t.datetime "start"
     t.datetime "over"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.integer  "user_id"
-    t.integer  "participants_count", default: 0
-    t.integer  "registery_fee",      default: 0
-    t.boolean  "check_arrival",      default: false
-    t.boolean  "show_participants",  default: true
-    t.index ["check_arrival"], name: "index_events_on_check_arrival", using: :btree
-    t.index ["name"], name: "index_events_on_name", using: :btree
-    t.index ["over"], name: "index_events_on_over", using: :btree
-    t.index ["sign_up_begin"], name: "index_events_on_sign_up_begin", using: :btree
-    t.index ["sign_up_end"], name: "index_events_on_sign_up_end", using: :btree
-    t.index ["start"], name: "index_events_on_start", using: :btree
-  end
-
-  create_table "oauths", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text     "token"
-    t.index ["provider", "uid"], name: "index_oauths_on_provider_and_uid", unique: true, using: :btree
-    t.index ["user_id"], name: "index_oauths_on_user_id", using: :btree
+    t.integer "user_id"
+    t.integer "participants_count", default: 0
+    t.integer "registery_fee", default: 0
+    t.boolean "check_arrival", default: false
+    t.boolean "show_participants", default: true
+    t.index ["check_arrival"], name: "index_events_on_check_arrival"
+    t.index ["name"], name: "index_events_on_name"
+    t.index ["over"], name: "index_events_on_over"
+    t.index ["sign_up_begin"], name: "index_events_on_sign_up_begin"
+    t.index ["sign_up_end"], name: "index_events_on_sign_up_end"
+    t.index ["start"], name: "index_events_on_start"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "sex"
-    t.date     "birth"
-    t.string   "phone"
-    t.string   "cellphone"
-    t.string   "address"
-    t.string   "education"
-    t.integer  "user_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "id_number"
-    t.string   "emergency_contact"
-    t.index ["birth"], name: "index_profiles_on_birth", using: :btree
-    t.index ["cellphone"], name: "index_profiles_on_cellphone", using: :btree
-    t.index ["id_number"], name: "index_profiles_on_id_number", unique: true, using: :btree
-    t.index ["name"], name: "index_profiles_on_name", using: :btree
-    t.index ["phone"], name: "index_profiles_on_phone", using: :btree
-    t.index ["sex"], name: "index_profiles_on_sex", using: :btree
-    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  create_table "oauths", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "token"
+    t.index ["provider", "uid"], name: "index_oauths_on_provider_and_uid", unique: true
+    t.index ["user_id"], name: "index_oauths_on_user_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "resource_id"
-    t.string   "resource_type"
+  create_table "profiles", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "sex"
+    t.date "birth"
+    t.string "phone"
+    t.string "cellphone"
+    t.string "address"
+    t.string "education"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "id_number"
+    t.string "emergency_contact"
+    t.index ["birth"], name: "index_profiles_on_birth"
+    t.index ["cellphone"], name: "index_profiles_on_cellphone"
+    t.index ["id_number"], name: "index_profiles_on_id_number", unique: true
+    t.index ["name"], name: "index_profiles_on_name"
+    t.index ["phone"], name: "index_profiles_on_phone"
+    t.index ["sex"], name: "index_profiles_on_sex"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "roles", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "resource_id"
+    t.string "resource_type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
-    t.index ["name"], name: "index_roles_on_name", using: :btree
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
   end
 
-  create_table "service_schedules", force: :cascade do |t|
-    t.integer  "service_team_id"
+  create_table "service_schedules", id: :serial, force: :cascade do |t|
+    t.integer "service_team_id"
     t.datetime "service_time"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["service_team_id"], name: "index_service_schedules_on_service_team_id", using: :btree
-    t.index ["service_time"], name: "index_service_schedules_on_service_time", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["service_team_id"], name: "index_service_schedules_on_service_team_id"
+    t.index ["service_time"], name: "index_service_schedules_on_service_time"
   end
 
-  create_table "service_team_users", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "role_id"
-    t.integer  "service_team_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["role_id"], name: "index_service_team_users_on_role_id", using: :btree
-    t.index ["service_team_id"], name: "index_service_team_users_on_service_team_id", using: :btree
-    t.index ["user_id"], name: "index_service_team_users_on_user_id", using: :btree
+  create_table "service_team_users", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.integer "service_team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_service_team_users_on_role_id"
+    t.index ["service_team_id"], name: "index_service_team_users_on_service_team_id"
+    t.index ["user_id"], name: "index_service_team_users_on_user_id"
   end
 
-  create_table "service_teams", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "schedules_count", default: 0, null: false
-    t.integer  "users_count",     default: 0, null: false
-    t.index ["name"], name: "index_service_teams_on_name", using: :btree
-    t.index ["user_id"], name: "index_service_teams_on_user_id", using: :btree
+  create_table "service_teams", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "schedules_count", default: 0, null: false
+    t.integer "users_count", default: 0, null: false
+    t.index ["name"], name: "index_service_teams_on_name"
+    t.index ["user_id"], name: "index_service_teams_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "subscriptions", force: :cascade do |t|
+    t.boolean "new_events", default: false
+    t.boolean "joined_events", default: true
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["joined_events"], name: "index_subscriptions_on_joined_events"
+    t.index ["new_events"], name: "index_subscriptions_on_new_events"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
   end
 
   add_foreign_key "announcements", "users"
@@ -183,4 +194,5 @@ ActiveRecord::Schema.define(version: 20171001133827) do
   add_foreign_key "service_team_users", "service_teams"
   add_foreign_key "service_team_users", "users"
   add_foreign_key "service_teams", "users"
+  add_foreign_key "subscriptions", "users"
 end
---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :subscription do
+    new_events false
+    joined_event false
+    user nil
+  end
+end
