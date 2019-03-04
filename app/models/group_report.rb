7077b771fad28eafafa23a30af469b468b3860ac
楊竑昕 <jk195417@gmail.com>
2019-03-04 15:16:44 +0800

    GroupReport remove valid for next_meeting_time and next_meeting_place

--- app/models/group_report.rb
+++ app/models/group_report.rb
@@ -2,6 +2,6 @@ class GroupReport < ApplicationRecord
   belongs_to :group, counter_cache: :reports_count
   belongs_to :creater, class_name: 'User', optional: true
 
-  validates_presence_of :adults_count, :children_count, :dedication, :next_meeting_time, :next_meeting_place
+  validates_presence_of :adults_count, :children_count, :dedication
   validates_numericality_of :adults_count, :children_count, :dedication
 end
