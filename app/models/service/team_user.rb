4a294483bcd038d5f43ee14ac95b021dcc5d665d
楊竑昕 <jk195417@gmail.com>
2019-03-08 16:55:54 +0800

    TeamUser user:require team:optional

--- app/models/service/team_user.rb
+++ app/models/service/team_user.rb
@@ -1,5 +1,5 @@
 class Service::TeamUser < ApplicationRecord
   belongs_to :user
-  belongs_to :team, foreign_key: 'service_team_id', counter_cache: :users_count
+  belongs_to :team, foreign_key: 'service_team_id', counter_cache: :users_count, optional: true
   belongs_to :role
 end
