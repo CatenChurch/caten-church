936e5d1c88afd9c3cb5ef58d3b271ad208e0754c
楊竑昕 <jk195417@gmail.com>
2019-03-18 18:50:47 +0800

    把  i18n default_locale 設定 搬到 initializer

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,5 @@ module CatenChurchWeb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Taipei'
-    config.i18n.default_locale = 'zh-TW'
   end
 end
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,3 +1,8 @@
-# load config/locales/**/*.yml
-# i18n yml can org by subfolder
-Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
+# Where the I18n library should search for translation files
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+# Whitelist locales available for the application
+I18n.available_locales = [:en, 'zh-TW']
+
+# Set default locale to something other than :en
+I18n.default_locale = 'zh-TW'
