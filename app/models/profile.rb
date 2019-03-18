3b7f515a252d1b839509857122d3080d5d377c39
楊竑昕 <jk195417@gmail.com>
2019-03-18 17:06:50 +0800

    profile.valid_for_joining_event == true 才驗證 id_number emergency_contact

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,13 @@
 class Profile < ApplicationRecord
+  attr_accessor :valid_for_joining_event
+
   belongs_to :user
 
-  validates_presence_of :name, :sex, :birth, :emergency_contact, :id_number
-  validates_format_of :id_number, with: /\A[a-z][1-2]\d{8}\z/i
+  validates_presence_of :name, :sex, :birth
   validates_acceptance_of :terms_of_service
+  # 有報名活動才填， profile.joining_event = true 來啟用驗證
+  validates_presence_of :id_number, :emergency_contact, if: :valid_for_joining_event
+  validates_format_of :id_number, with: /\A[a-z][1-2]\d{8}\z/i, if: :valid_for_joining_event
 
   def self.birth_month(month = nil)
     return all if month.blank?
