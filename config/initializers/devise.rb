9c377b9764f5af52eb84380a007a5aee43b2c3f0
楊竑昕 <jk195417@gmail.com>
2019-04-23 00:40:14 +0800

    Upgrade Facebook Login to Graph API v3.2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,8 +243,11 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook,
                   Rails.application.credentials[:facebook][:app_id],
-                  Rails.application.credentials[:facebook][:app_secret]
-
+                  Rails.application.credentials[:facebook][:app_secret],
+                  client_options: {
+                    site: 'https://graph.facebook.com/v3.2',
+                    authorize_url: 'https://www.facebook.com/v3.2/dialog/oauth'
+                  }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
