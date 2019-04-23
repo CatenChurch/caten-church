ad4aef86741b4bd6738df83c87f5caac69eadf70
楊竑昕 <jk195417@gmail.com>
2019-04-23 22:00:57 +0800

    Move i18n setting to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -15,6 +17,13 @@ module CatenChurchWeb
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Time zone
     config.time_zone = 'Taipei'
+    
+    # I18n
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, 'zh-TW']
+    config.i18n.default_locale = 'zh-TW'
   end
 end
--- config/initializers/i18n.rb
+++
@@ -1,8 +0,0 @@
-# Where the I18n library should search for translation files
-I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-
-# Whitelist locales available for the application
-I18n.available_locales = [:en, 'zh-TW']
-
-# Set default locale to something other than :en
-I18n.default_locale = 'zh-TW'
