342b357330e08eca58dacc03abf6eceaf06404f6
楊竑昕 <jk195417@gmail.com>
2019-06-03 19:22:24 +0800

    Delete i18n and focus on Chinese (Font-stage)

--- app/helpers/oauths_helper.rb
+++ app/helpers/oauths_helper.rb
@@ -2,24 +2,24 @@ module OauthsHelper
   def connect_facebook_link(user)
     if user.facebook_oauth.present?
       link_to "https://www.facebook.com/#{current_user.facebook_oauth.uid}", target: :_blank, class: 'btn btn-outline-primary list-group-item' do
-        icon 'fab', 'facebook', t('.oauths_helper.success'), class: 'fa-3x'
+        icon 'fab', 'facebook', '已連結', class: 'fa-3x'
       end
     else
       link_to user_facebook_omniauth_authorize_path, class: 'btn btn-outline-primary list-group-item' do
-        icon 'fab', 'facebook', t('.oauths_helper.none'), class: 'fa-3x'
+        icon 'fab', 'facebook', '尚未連結', class: 'fa-3x'
       end
     end
   end
 
   def connect_google_link(_user)
     link_to '#google-login-is-coming', class: 'btn list-group-item disabled' do
-      icon 'fab', 'google', t('.oauths_helper.pending'), class: 'fa-3x'
+      icon 'fab', 'google', '即將推出', class: 'fa-3x'
     end
   end
 
   def connect_instagram_link(_user)
     link_to '#instagram-login-is-coming', class: 'btn list-group-item disabled' do
-      icon 'fab', 'instagram', t('.oauths_helper.pending'), class: 'fa-3x'
+      icon 'fab', 'instagram', '即將推出', class: 'fa-3x'
     end
   end
 end
