6ea892f6dbdf7cb8f9d05fd43d2218be2a2f5204
楊竑昕 <jk195417@gmail.com>
2020-01-27 12:22:42 +0800

    Update mailer, change sender to '茄典教會 <notify@caten-church.org>'

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,4 +1,6 @@
 class EventMailer < ApplicationMailer
+  default from: '茄典教會 <notify@caten-church.org>'
+
   def opening_notice(event, recipient)
     @event = event
     @recipient = recipient
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ApplicationMailer
+  default from: '茄典教會 <notify@caten-church.org>'
+
   def birth_notification(recipient, month)
     @recipient = recipient
     @month = month
-
     @users = User.birth_month(month).includes(:profile)
     @days = Array.new(31) { [] }
     @users.each { |user| @days[user.profile.birth.day] << user }
-
     mail to: @recipient.email, subject: "茄典教會會員 #{@month} 月份壽星通知"
   end
 end
