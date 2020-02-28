0f8522c2e0d8c856d1b752177dccf4d3557406f4
楊竑昕 <jk195417@gmail.com>
2020-02-28 23:59:43 +0800

    Update active-nav.js

--- app/javascript/packs/active-nav.js
+++ app/javascript/packs/active-nav.js
@@ -1,9 +1,9 @@
 document.addEventListener('turbolinks:load', () => {
   const path = window.location.pathname.split('/')
-  // frontstage
+  // front stage
   let nodes = document.querySelectorAll(`a[data-active='${path[1]}']`)
   nodes.forEach(node => node.classList.add('active'))
-  // account/admin/users
+  // account/admin/users stage
   nodes = document.querySelectorAll(`a[data-active='${path[1]}-${path[2]}']`)
   nodes.forEach(node => node.classList.add('active'))
 })
