93adcd0e079adcaa443bd601661116ca4e176884
楊竑昕 <jk195417@gmail.com>
2020-05-02 15:08:35 +0800

    Improve weekly validations

--- app/models/weekly.rb
+++ app/models/weekly.rb
@@ -4,20 +4,35 @@ class Weekly < ApplicationRecord
   has_one_attached :pdf
   validates_presence_of :title, :issue, :published_at
   validates_numericality_of :issue
-  validate :pdf_validations
+  validate :attached_validations
 
   private
 
-  def pdf_validations
-    errors.add(:pdf, '需附加檔案') && return unless pdf.attached?
+  def attached_validations
+    attached_presence_validated? && attached_type_validated? && attached_size_validated?
+  end
+
+  def attached_presence_validated?
+    return true if pdf.attached?
+
+    errors.add(:pdf, '需附加檔案')
+    false
+  end
+
+  def attached_type_validated?
     accept_types = %w[application/pdf]
-    unless accept_types.include? pdf.content_type
-      errors.add(:pdf, "檔案類型須為 application/pdf, 目前是 #{pdf.content_type}")
-    end
+    return true if accept_types.include? pdf.content_type
+
+    errors.add(:pdf, "檔案類型須為 application/pdf, 目前是 #{pdf.content_type}")
+    false
+  end
+
+  def attached_size_validated?
     max_size = 3.megabytes
-    if pdf.byte_size > max_size
-      errors.add(:pdf, "檔案須小於 #{human_size max_size}, 目前是 #{human_size pdf.byte_size}")
-    end
+    return true if pdf.byte_size <= max_size
+
+    errors.add(:pdf, "檔案須小於 #{human_size max_size}, 目前是 #{human_size pdf.byte_size}")
+    false
   end
 
   def human_size(bytes)
